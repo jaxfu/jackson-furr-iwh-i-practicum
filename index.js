702ddf7dc96feb2d274b145f688a121373616ddf
Jackson Furr <jackson.a.furr@gmail.com>
2025-07-29 22:53:19 -0400

    post test

--- index.js
+++ index.js
@@ -25,7 +25,22 @@ app.get("/update-cobj", async (req, res) => {
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
-// * Code for Route 3 goes here
+app.post("/update-cobj", async (req, res) => {
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    "Content-Type": "application/json",
+  };
+
+  const { name, ram, storage } = req.body;
+  console.log(name, ram, storage);
+
+  // try {
+  //   await axios.patch(updateContact, update, { headers });
+  //   res.redirect("back");
+  // } catch (err) {
+  //   console.error(err);
+  // }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
